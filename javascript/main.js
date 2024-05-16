68c615849d101249b62679f6880ac19b4b3275ec
bowie <axfrontend@gmail.com>
2024-05-16 10:27:46 -0300

    commit
    construção nav

--- javascript/main.js
+++ javascript/main.js
@@ -5,15 +5,12 @@ function preLoad(){
         preloadContent.style.animation='fadeOut'
         site.style.animationDuration='1s'
         
-       console.log('on')
     }, 1000); 
     setTimeout(() => {
         site.style.animation='fadeIn'
         site.style.animationDuration='1s'
         site.style.display='block'
         preloadContent.remove()
-        
-       console.log('on')
     }, 3000);
 }
 
