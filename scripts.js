cbee10fe1b6de708dbb952852dd03a06d649d117
Alex Rocha <131891153+Axvand@users.noreply.github.com>
2025-03-31 17:57:17 -0300

    Construção e manutenção
    Manutenção do menu dropdown moile

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navLinks = document.querySelector(".nav-links");
-    const navLinksItems = document.querySelectorAll(".nav-links a");
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+  const navLinksItems = document.querySelectorAll(".nav-links a");
 
-    menuToggle.addEventListener("click", function () {
-      navLinks.classList.toggle("active");
-      menuToggle.classList.toggle("active");
-    });
+  menuToggle.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+    menuToggle.classList.toggle("active");
+  });
 
-    // Fecha o menu ao clicar em um link
-    navLinksItems.forEach((link) => {
-      link.addEventListener("click", () => {
-        navLinks.classList.remove("active");
-        menuToggle.classList.remove("active");
-      });
+  // Fecha o menu ao clicar em um link
+  navLinksItems.forEach((link) => {
+    link.addEventListener("click", () => {
+      navLinks.classList.remove("active");
+      menuToggle.classList.remove("active");
     });
   });
+});
